8e4b37f71d16bab98288d17bb374dcc8da40c91f
mario-mra <mario.hi52@gmail.com>
2017-10-29 01:55:15 +0200

    Actualizacion de memoria

--- FPGA/modules/memory/memory.v
+++ FPGA/modules/memory/memory.v
@@ -8,9 +8,9 @@
 /*
   Inputs:
     - CS   (Chip Select)[1]:         Enable access to memory module.
-    - WE   (Write Enable)[1]:        On Posedge, perform a memory write.
-    - RE   (Read Enable)[1]:         On Posedge, perform a memory read.
-    - SE   (Shift Enable)[1]:        On Posedge, perform a memory shift.
+    - PW   (Perform Write)[1]:       On Posedge, perform a memory write.
+    - PR   (Perform Read)[1]:        On Posedge, perform a memory read.
+    - PS   (Perform Shift)[1]:       On Posedge, perform a memory shift.
     - SD   (Shift Direction)[1]:     Control the direction of the memory shift.
     - ADDR (Address Bus)[Addr_size]: Contain the memory address.
 
@@ -25,9 +25,12 @@
     - Mem_size:  Size of the memory.
 */
 
-module ram #(parameter addrs = 4,
-             parameter block_size = 8)
-            (inout [size-1:0]data,
-             input clk);
+module ram #(parameter Addr_size = 3,
+             parameter Mem_size = 3)
+            ();
 
+  initial begin
+    $display("%0d %0d", $clog(3), $clog10(3));
+    #1 $finish;
+  end
 endmodule
