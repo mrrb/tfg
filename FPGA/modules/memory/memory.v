71aa1666b672caf117551794a5e55dadf4b4f4bd
mario-mra <mario.hi52@gmail.com>
2017-11-04 16:35:56 +0100

    Cambios

--- FPGA/modules/memory/memory.v
+++ FPGA/modules/memory/memory.v
@@ -1,4 +1,4 @@
-// Modulo de memoria RAM
+// Modulo de memoria.
 // Mario Rubio.
 
 // Little-endian [N:0] or Big-endian [0:N]??
@@ -16,8 +16,9 @@
     - ADDR (Address Bus)[Addr_size>wire]: Contain the memory address.
 
   Outputs:
-    - ERR  (Error)[1]:            Error detected.
-    - SVO  (Shift Value Old)[1>wire]:
+    - ERR    (Error)[1]:            Error detected.
+    - SVO    (Shift Value Old)[1>wire]:
+    - DEBUG  (DEBUG)[1>wire]:
 
   Inouts:
     - DATA (Data Bus)[Mem_size]: IN/OUT of data.
@@ -30,7 +31,7 @@
   Read
     1. Put CS input HIGH.
     2. Select the memory position with the address bus.
-    3. Put PR input HIGH to obtain the values wanted on the DATA bus, once you have finished, put the LOW the input again to free the bus.
+    3. Put PR input HIGH to obtain the values wanted on the DATA bus, once you have finished, put LOW the input again to free the bus.
 
   Write
     1. Put CS input HIGH.
@@ -44,16 +45,38 @@
     3. Select the direction of the shift with the SD input (HIGH > Right, LOW > Left).
     4. Pulse PS input to perform a Shift action, the deleted BIT will be outputted on the SVO pin.
 */
-module memory #(parameter Addr_width = 8,
+
+// Not valid > E > NOT SUPPORTED IN YOSYS!
+// Only return 1 if the 2 first imputs are HIGH and the others LOW
+// primitive func_selector (output OUT, input I1, input I2, input I3, input I4);
+//   table
+//     1 1 0 0 : 1;
+//     0 ? ? ? : 0;
+//     1 0 ? ? : 0;
+//   endtable
+// endprimitive
+
+module memory #(parameter Addr_width = 3,
                 parameter Data_width = 8)
                (input wire CS,
                 input wire PW,
                 input wire PR,
                 input wire PS,
                 input wire SD,
+                input wire SV,
                 input wire [Addr_width-1:0]ADDR,
                 output wire ERR,
+                output wire DEBUG,
                 inout wire [Data_width-1:0]DATA);
+
+
+  // Initial status
+  // ToDo
+  reg [Data_width-1:0] memory [(2**Addr_width)-1:0]; // Memory block
+
+  initial begin
+    memory [1] = 8'b00001111;
+  end
 
   // Control logic
   wire S, R, W;
@@ -61,19 +84,20 @@ module memory #(parameter Addr_width = 8,
   assign R = (CS & ~PW) & (PR & ~PS);
   assign W = (CS & PW) & (~PR & ~PS);
 
+  assign DEBUG = R;
+
   // Memory Read
+  // Not Valid > W > Tri-states gates support limited with Yosys
+  // assign DATA = R?memory[ADDR]:8'bZZZZZZZZ;
   // ToDo
 
   // Memory Write
-  // ToDo
+  always @ ( posedge W ) begin
+    memory[ADDR] = DATA;
+  end
 
   // Memory Shift
   // ToDo
 
-  // Initial status
-  // ToDo
-  initial begin
-
-  end
 
 endmodule
---
+++ FPGA/modules/memory/memory_tb.v
@@ -0,0 +1,68 @@
+// Modulo de memoria. Test Bench.
+// Mario Rubio.
+
+// `include "memory.v"
+
+module memory_tb ();
+
+  parameter Addr_width = 3;
+  parameter Data_width = 8;
+
+  reg CS, PW, PR, PS, SD, SV;
+  wire ERR;
+  wire DEBUG;
+
+  reg [Addr_width-1:0]ADDR;
+  wire [Data_width-1:0]DATA;
+
+  memory #(.Addr_width(Addr_width),
+           .Data_width(Data_width))
+          mem
+          (.CS(CS),
+           .PW(PW),
+           .PR(PR),
+           .PS(PS),
+           .SD(SD),
+           .SV(SV),
+           .ADDR(ADDR),
+           .ERR(ERR),
+           .DEBUG(DEBUG),
+           .DATA(DATA));
+
+  // initial begin
+  //   $monitor("CS=%0d PR=%0d OUT=%0d", CS, PR, DEBUG);
+  //   CS = 0;
+  //   PR = 0;
+  //   PS = 0;
+  //   PW = 0;
+  //   #1 CS = 1;
+  //   #1 PR = 1;
+  //   #1 PR = 0;
+  //   #1 PR = 1;
+  //   #1 CS = 0;
+  //   #1 $finish;
+  // end
+  reg [Data_width-1:0]ctrl = 8'bzzzzzzzz;
+  assign DATA = ctrl;
+  initial begin
+    $display("%d", {{ (3){1'bZ} }, 1'bZ } );
+    CS = 0; PW = 0; PR = 0; PS = 0; SD = 0; SV = 0;
+    $monitor("CS=%b, PR=%b, PW=%b, PS=%b, SD=%b, SV=%b, DATA=%b, ADDR=%b", CS, PR, PW, PS, SD, SV, DATA, ADDR);
+    $display("Memory Read!");
+    #1 CS = 1; ADDR = 3'b001;
+    #1 PR = 1;
+
+    #1 CS = 0; PW = 0; PR = 0; PS = 0; SD = 0; SV = 0;
+    $display("Memory Write!");
+    #1 CS = 1; ADDR = 3'b001; ctrl = 8'b10100101;
+    #1 PW = 1;
+    #1 ctrl = 8'bzzzzzzzz;
+    #1 PW = 0;
+
+    #1 PR = 1;
+
+    #1 CS = 0; PW = 0; PR = 0; PS = 0; SD = 0; SV = 0;
+    $display("Memory Shift!");
+  end
+
+endmodule
