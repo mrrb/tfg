1c3e143d9e636cc292e3015799ae394c334d93f0
mario-mra <mario.hi52@gmail.com>
2018-11-19 21:15:26 +0100

    Fix BRAM initialization in FIFO BRAM SYNC module

--- ICEstick/USB3300_sniffer/modules/FIFO_BRAM_SYNC/rtl/FIFO_BRAM_SYNC_.v
+++ ICEstick/USB3300_sniffer/modules/FIFO_BRAM_SYNC/rtl/FIFO_BRAM_SYNC_.v
@@ -4,10 +4,10 @@
  * This module genereates a SYNCHRONOUS FIFO memory based on a BRAM.
  *
  * Parameters:
- *  - ALMOST_FULL. Ratio* that makes wr_almost_full HIGH (ALMOST_FULL to 1). [Default: 0.9]
- *  - ALMOST_EMPTY. Ratio* that makes rd_almost_empty LOW (0 to ALMOST_EMPTY). [Default: 0.1]
+ *  - DATA_WIDTH. Size of each cell. [Default: FIFO_BRAM_8 bits]
+ *  - ALMOST_FULL. Ratio that makes wr_almost_full HIGH (ALMOST_FULL to 1). [Default: 0.9]
+ *  - ALMOST_EMPTY. Ratio that makes rd_almost_empty LOW (0 to ALMOST_EMPTY). [Default: 0.1]
  *  - FWFT_MODE. First Word Fall Through (1 > enable, 0 > disable). [ToDo]
- *  *Value = RATIO*BRAM_SIZE
  *
  * Inputs:
  *  - rst. Reset signal, active LOW
@@ -39,9 +39,10 @@
 `define FIFO_BRAM_2  2
 
 module FIFO_BRAM_SYNC #(
+                        parameter DATA_WIDTH   = `FIFO_BRAM_8,
                         parameter ALMOST_FULL  = 0.9,
                         parameter ALMOST_EMPTY = 0.1,
-                        parameter FWFT_MODE    = 1    // ToDo
+                        parameter FWFT_MODE    = 1 // ToDo
                        )
                        (
                         // System signals
@@ -49,30 +50,29 @@ module FIFO_BRAM_SYNC #(
                         input  wire clk, // Reference clock
 
                         // Write control signals
-                        input  wire wr_dv, // Write Data Valid control signal (active HIGH)
+                        input  wire wr_dv,  // Write Data Valid
 
                         // Write data signals
                         input  wire [(DATA_WIDTH-1):0]wr_DATA, // Input DATA
                         
                         // Write flags
-                        output wire wr_full,        // The FIFO memory is full flag
-                        output wire wr_almost_full, // The FIFO memory is almost full flag
+                        output wire wr_full,        // The FIFO memory is full
+                        output wire wr_almost_full, // The FIFO memory is almost full
 
                         // Read control signals            
-                        input  wire rd_en, // Read enable control signal (active HIGH)
+                        input  wire rd_en,  // Read enable
 
                         // Read data signals            
                         output wire [(DATA_WIDTH-1):0]rd_DATA, // Output DATA
                         
                         // Read flags
-                        output wire rd_empty,       // The FIFO memory is empty flag
-                        output wire rd_almost_empty // The FIFO memory is almost empty flag
+                        output wire rd_empty,       // The FIFO memory is empty
+                        output wire rd_almost_empty // The FIFO memory is almost empty
                        );
 
-    localparam DATA_WIDTH = `FIFO_BRAM_8;         // Size of each cell [Default: 8-bits]
-    localparam BRAM_MODE  = 4-$clog2(DATA_WIDTH); // ICE40 BRAM mode
-    localparam BRAM_SIZE  = 256*(2**BRAM_MODE);   // Maximum number of bytes that can be stored in the FIFO
-    localparam ADDR_SIZE  = $clog2(BRAM_SIZE);    // Bits of the address
+    localparam BRAM_MODE = 4-$clog2(DATA_WIDTH); // BRAM mode
+    localparam BRAM_SIZE = 256*(2**BRAM_MODE);   // Maximum number of bytes that can be stored in the FIFO
+    localparam ADDR_SIZE = $clog2(BRAM_SIZE);    // Bits of the address
 
     localparam ALMOST_FULL_VAL  = $ceil(BRAM_SIZE*ALMOST_FULL);  // Minimum value that makes wr_almost_full HIGH.
     localparam ALMOST_EMPTY_VAL = $ceil(BRAM_SIZE*ALMOST_EMPTY); // Maximum value that makes rd_almost_empty HIGH.
@@ -84,61 +84,55 @@ module FIFO_BRAM_SYNC #(
     /// End of Error control
 
     /// BRAM init
-    wire [(DATA_WIDTH-1):0]BRAM_DATA_wr, BRAM_DATA_rd; // ICE40 BRAM ADDRESSES after 
-    wire  [(ADDR_SIZE-1):0]BRAM_ADDR_wr, BRAM_ADDR_rd; // ICE40 BRAM DATA after the selection of the wanted bits from the BRAM
+    wire [(DATA_WIDTH-1):0]BRAM_DATA_wr;
+    wire [15:0]BRAM_DATA_rd;
+    wire  [(ADDR_SIZE-1):0]BRAM_ADDR_wr, BRAM_ADDR_rd;
     wire BRAM_WE, BRAM_RE;
-
-    wire [(DATA_WIDTH-1):0]DW, DR;  // Shorter names for the DATA I/O
-    wire [(DATA_WIDTH-1):0]_NULLR_; // Null vector for the READ DATA
-    assign DW = BRAM_DATA_wr; // Input assignment
-    assign BRAM_DATA_rd = DR; // Output assignment
     SB_RAM40_4K #(
                   .WRITE_MODE(BRAM_MODE), // DATA Width
                   .READ_MODE(BRAM_MODE)   // DATA Width
                  )
     FIFO_BRAM    (
                   // WRITE signals
-                  .WDATA({1'hx, DW[7], 1'hx, DW[6], 1'hx, DW[5], 1'hx, DW[4],
-                          1'hx, DW[3], 1'hx, DW[2], 1'hx, DW[1], 1'hx, DW[0]}), // [Input, 16bits]
-                  .WADDR({2'b0, BRAM_ADDR_wr}), // [Input, 11bits]
-                  .WE(BRAM_WE),                 // [Input]
-                  .WCLK(clk),                   // [Input]
-                  .WCLKE(BRAM_WE),              // [Input]
+                  .WDATA({8'b0,BRAM_DATA_wr}), // [Input]
+                  .WADDR({2'b0,BRAM_ADDR_wr}), // [Input]
+                  .WE(BRAM_WE),                // [Input]
+                  .WCLK(clk),                  // [Input]
+                  .WCLKE(BRAM_WE),             // [Input]
 
                   // READ signals
-                  .RDATA({_NULLR_[7], DR[7], _NULLR_[6], DR[6], _NULLR_[5], DR[5], _NULLR_[4], DR[4],
-                          _NULLR_[3], DR[3], _NULLR_[2], DR[2], _NULLR_[1], DR[1], _NULLR_[0], DR[0]}), // [Output, 16bits]
-                  .RADDR({2'b0, BRAM_ADDR_rd}), // [Input, 11bits]
-                  .RE(BRAM_RE),                 // [Input]
-                  .RCLK(!clk),                  // [Input]
-                  .RCLKE(BRAM_RE)               // [Input]
+                  .RDATA(BRAM_DATA_rd),        // [Output]
+                  .RADDR({2'b0,BRAM_ADDR_rd}), // [Input]
+                  .RE(BRAM_RE),                // [Input]
+                  .RCLK(!clk),                 // [Input]
+                  .RCLKE(BRAM_RE)              // [Input]
                  );
     /// End of BRAM init
 
     /// Regs and wires
-    reg [ADDR_SIZE:0]FIFO_size = 0;   // Number of bytes stored in the FIFO
+    reg [ADDR_SIZE:0]FIFO_size = 0;
 
     reg [(ADDR_SIZE-1):0]ADDR_wr = 0; // Address where the DATA is going to be add
     reg [(ADDR_SIZE-1):0]ADDR_rd = 0; // Address where the DATA is going to be read
 
-    wire wr_ctrl, rd_ctrl; // Control signals that go HIGH whenever a WRITE/READ action can occur
+    wire wr_ctrl, rd_ctrl;
     /// End of Regs and wires
 
     /// Assigns
-    assign BRAM_DATA_wr = wr_DATA; // Input assign
-    assign rd_DATA = BRAM_DATA_rd; // Output assign
+    assign BRAM_DATA_wr = wr_DATA;
+    assign rd_DATA = BRAM_DATA_rd[7:0];
 
-    assign BRAM_ADDR_wr = ADDR_wr; // BRAM WRITE address
-    assign BRAM_ADDR_rd = ADDR_rd; // BRAM READ address
+    assign BRAM_ADDR_wr = ADDR_wr;
+    assign BRAM_ADDR_rd = ADDR_rd;
 
-    assign wr_ctrl = wr_dv && !wr_full;  // The FIFO ONLY can be WRITE when It isn't full AND wr_dv is HIGH
-    assign rd_ctrl = rd_en && !rd_empty; // The FIFO ONLY can be READ when It isn't empty AND rd_en is HIGH
+    assign wr_ctrl = wr_dv && !wr_full;
+    assign rd_ctrl = rd_en && !rd_empty;
 
-    assign wr_full  = (FIFO_size == BRAM_SIZE) ? 1'b1 : 1'b0; // Full flag
-    assign rd_empty = (FIFO_size == 0)         ? 1'b1 : 1'b0; // Empty flag
+    assign wr_full  = (FIFO_size == BRAM_SIZE) ? 1'b1 : 1'b0;
+    assign rd_empty = (FIFO_size == 0)         ? 1'b1 : 1'b0;
 
-    assign wr_almost_full  = (FIFO_size >= ALMOST_FULL_VAL)  ? 1'b1 : 1'b0; // Almost full flat
-    assign rd_almost_empty = (FIFO_size <= ALMOST_EMPTY_VAL) ? 1'b1 : 1'b0; // Almost empty flag
+    assign wr_almost_full  = (FIFO_size >= ALMOST_FULL_VAL) ? 1'b1 : 1'b0;
+    assign rd_almost_empty = (FIFO_size <= ALMOST_EMPTY_VAL)  ? 1'b1 : 1'b0;
     /// End of Assigns
 
     /// Controller
@@ -150,21 +144,20 @@ module FIFO_BRAM_SYNC #(
                                                                      // Otherwise, both push & pull OR no change
     end
 
-    // WRITE address controller
+    // Address controller
     always @(posedge clk `FIFO_BRAM_SYNC_ASYNC_RESET) begin
         if(!rst) ADDR_wr <= 0;
         else if(wr_ctrl) ADDR_wr <= ADDR_wr + 1'b1; // FIFO push
     end
 
-    // READ address controller
     always @(posedge clk `FIFO_BRAM_SYNC_ASYNC_RESET) begin
         if(!rst) ADDR_rd <= 0;
         else if(rd_ctrl) ADDR_rd <= ADDR_rd + 1'b1; // FIFO pull
     end
 
     // FIFO controller
-    assign BRAM_WE = (wr_ctrl) ? 1'b1 : 1'b0;
-    assign BRAM_RE = 1'b1;
+    assign BRAM_WE = (wr_ctrl)   ? 1'b1 : 1'b0;
+    assign BRAM_RE = (!rd_empty) ? 1'b1 : 1'b0;
     /// End of Controller
 
 endmodule
