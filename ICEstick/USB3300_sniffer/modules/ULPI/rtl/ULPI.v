eb2ba2bbf7585e9f0d51bcd614339f08d612fdfd
mario-mra <mario.hi52@gmail.com>
2018-11-22 00:09:44 +0100

    [WIP] Add support for the ULPI_REG_WRITE module

--- ICEstick/USB3300_sniffer/modules/ULPI/rtl/ULPI.v
+++ ICEstick/USB3300_sniffer/modules/ULPI/rtl/ULPI.v
@@ -16,6 +16,8 @@
     `define ULPI_ASYNC_RESET
 `endif
 
+`include "./modules/ULPI_RW.vh"
+
  module ULPI (
               // System signals
               input  wire rst,       // Reset signal
@@ -23,7 +25,12 @@
               input  wire clk_ULPI,  // 60MHz ULPI clock
 
               // Control signals
+              input  wire PrW,
 
+              // Register signals
+              input  wire [5:0]ADDR,
+              input  wire [7:0]REG_VAL_W,
+              output wire [7:0]REG_VAL_R,
 
               // ULPI signals
               input  wire DIR,       // ULPI DIR (DIRection) signal
@@ -33,30 +40,57 @@
               output wire U_RST      // ULPI RST (ReSeT) signal
              );
 
-    // /// DATA inout selector
-    //     wire [7:0]DATA_out; // Data generated in the FPGA (link)
-    //     assign DATA = (DIR) ? 8'bz : DATA_out; // When the PHY has the ownership of the bus, the FPGA must have the inputs in a HIGH impedance mode
-    //                                            // so there will not have any conflicts.
-    // /// End of DATA inout selector
+    /// ULPI reset
+    assign U_RST = !rst;
+    /// End of ULPI reset
 
-    // /// Metastability solver
-    // reg [7:0]DATA_A, DATA_B;
-    // wire [7:0]DATA_in;
-    // assign DATA_in = DATA_B;
+    /// DATA inout selector
+    wire [7:0]DATA_out; // Data generated in the FPGA (link)
+    assign DATA = (DIR) ? 8'bz : DATA_out; // When the PHY has the ownership of the bus, the FPGA must have the inputs in a HIGH impedance mode
+                                            // so there will not have any conflicts.
+    /// End of DATA inout selector
 
-    // always @(posedge clk_ULPI `ULPI_ASYNC_RESET) begin
-    //     if(!rst) begin
-    //         DATA_A <= 0;
-    //         DATA_B <= 0;
-    //     end
-    //     else if(ULPI_DIR) begin
-    //         DATA_A <= DATA;
-    //         DATA_B <= DATA_A;
-    //     end
-    // end
+    /// Metastability solver
+    // Check the reference doc FPGA_ICE40/wp-01082-quartus-ii-metastability.pdf for more info
+    // (Or https://www.intel.com/content/dam/www/programmable/us/en/pdfs/literature/wp/wp-01082-quartus-ii-metastability.pdf)
+    reg [7:0]DATA_A, DATA_B;
+    wire [7:0]DATA_in;
+    assign DATA_in = DATA_B;
+
+    always @(posedge clk_ULPI `ULPI_ASYNC_RESET) begin
+        if(!rst) begin
+            DATA_A <= 0;
+            DATA_B <= 0;
+        end
+        else if(DIR) begin
+            DATA_A <= DATA;
+            DATA_B <= DATA_A;
+        end
+    end
     /// End of Metastability solver
 
     /// Reg_Write submodule init
+    wire RW_busy;
+    ULPI_REG_WRITE ULPI_RW (
+                            // System signals
+                            .rst(rst),           // [Input]
+                            .clk_ULPI(clk_ULPI), // [Input]
+
+                            // Control signals
+                            .PrW(PrW),           // [Input]
+                            .busy(RW_busy),      // [Output]
+
+                            // Register values
+                            .ADDR(ADDR),         // [Input]
+                            .REG_VAL(REG_VAL_R), // [Input]
+
+                            // ULPI signals
+                            .DIR(DIR),           // [Input]
+                            .NXT(NXT),           // [Input]
+                            .DATA_I(DATA_in),    // [Input]
+                            .DATA_O(DATA_out),   // [Output]
+                            .STP(STP)            // [Output]
+                           );
     /// End of Reg_Write submodule init
 
     /// Reg_Read submodule init
