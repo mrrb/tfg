0fc639ee1153d30aacbf904430dcc5b752b815f3
Mario Rubio <mario.mra@hotmail.com>
2019-02-01 21:09:25 +0100

    Add main controller module

---
+++ ICEstick/USB3300_sniffer/modules/main_controller/rtl/main_controller.v
@@ -0,0 +1,261 @@
+/*
+ *
+ * main_controller module
+ * main_controller module let 
+ *
+ * Inputs:
+ *  - 
+ *
+ * Outputs:
+ *  - 
+ *
+ */
+
+`default_nettype none
+
+`ifdef ASYNC_RESET
+    `define MAIN_CONTROLLER_ASYNC_RESET or negedge rst
+`else
+    `define MAIN_CONTROLLER_ASYNC_RESET
+`endif
+
+module main_controller (
+                        // System signals
+                        input  wire rst,            // Reset signal
+                        input  wire clk,            // Reference clock input
+                        input  wire force_send,
+
+                        // Op stack
+                        input  wire [15:0] op_stack_msg,
+                        input  wire op_stack_empty,                        
+                        output wire op_stack_pull,
+
+                        // General ULPI signals
+                        input  wire ULPI_busy,
+                        
+                        // DATA ULPI signals
+                        input  wire [7:0] ULPI_USB_DATA,
+                        input  wire [15:0] ULPI_USB_INFO_DATA,
+                        input  wire ULPI_DATA_buff_empty,
+                        input  wire ULPI_INFO_buff_empty,
+                        output wire ULPI_DATA_re,
+                        output wire ULPI_INFO_re,
+
+                        // Register ULPI signals
+                        input  wire [7:0] ULPI_REG_VAL_R,
+                        output wire [7:0] ULPI_REG_VAL_W,
+                        output wire [5:0] ULPI_ADDR,
+                        output wire ULPI_PrW,
+                        output wire ULPI_PrR,
+
+                        // UART
+                        input  wire UART_Tx_FULL,
+                        output wire [7:0] UART_Tx_DATA,
+                        output wire UART_send
+                       );
+
+    /// TOP controller
+    // TOP Regs and wires
+    // Control registers and wires
+    reg [3:0] MAIN_state_r = 0; // Register that stores the current TOP state
+    reg [1:0] MAIN_cmd_r = 0;
+
+    reg [5:0] ULPI_ADDR_r = 0;
+    reg [7:0] ULPI_REG_VAL_W_r = 0;
+
+    reg [5:0] RxCMD_r = 0;
+    reg [1:0] INFO_count_r = 0;
+    reg [9:0] DATA_count_r = 0;
+
+    reg [7:0] UART_Tx_DATA_r = 0;
+
+    // Flags
+    wire MAIN_s_IDLE;       // HIGH if MAIN_state_r == MAIN_IDLE,       else LOW
+    wire MAIN_s_REG_READ;   // HIGH if MAIN_state_r == MAIN_REG_READ,   else LOW
+    wire MAIN_s_REG_WRITE;  // HIGH if MAIN_state_r == MAIN_REG_WRITE,  else LOW
+    wire MAIN_s_REG_WAIT1;  // HIGH if MAIN_state_r == MAIN_REG_WAIT1,  else LOW
+    wire MAIN_s_REG_SEND;   // HIGH if MAIN_state_r == MAIN_REG_SEND,   else LOW
+    wire MAIN_s_REG_WAIT2;  // HIGH if MAIN_state_r == MAIN_REG_WAIT2,  else LOW
+    wire MAIN_s_RECV;       // HIGH if MAIN_state_r == MAIN_RECV,       else LOW
+    wire MAIN_s_RECV_SEND1; // HIGH if MAIN_state_r == MAIN_RECV_SEND1, else LOW
+    wire MAIN_s_RECV_WAIT;  // HIGH if MAIN_state_r == MAIN_RECV_WAIT,  else LOW
+    wire MAIN_s_RECV_SEND2; // HIGH if MAIN_state_r == MAIN_RECV_SEND2, else LOW
+
+    // Assigns
+    assign MAIN_s_IDLE       = (MAIN_state_r == MAIN_IDLE)       ? 1'b1 : 1'b0; // #FLAG
+    assign MAIN_s_REG_READ   = (MAIN_state_r == MAIN_REG_READ)   ? 1'b1 : 1'b0; // #FLAG
+    assign MAIN_s_REG_WRITE  = (MAIN_state_r == MAIN_REG_WRITE)  ? 1'b1 : 1'b0; // #FLAG
+    assign MAIN_s_REG_WAIT1  = (MAIN_state_r == MAIN_REG_WAIT1)  ? 1'b1 : 1'b0; // #FLAG
+    assign MAIN_s_REG_SEND   = (MAIN_state_r == MAIN_REG_SEND)   ? 1'b1 : 1'b0; // #FLAG
+    assign MAIN_s_REG_WAIT2  = (MAIN_state_r == MAIN_REG_WAIT2)  ? 1'b1 : 1'b0; // #FLAG
+    assign MAIN_s_RECV       = (MAIN_state_r == MAIN_RECV)       ? 1'b1 : 1'b0; // #FLAG
+    assign MAIN_s_RECV_SEND1 = (MAIN_state_r == MAIN_RECV_SEND1) ? 1'b1 : 1'b0; // #FLAG
+    assign MAIN_s_RECV_WAIT  = (MAIN_state_r == MAIN_RECV_WAIT)  ? 1'b1 : 1'b0; // #FLAG
+    assign MAIN_s_RECV_SEND2 = (MAIN_state_r == MAIN_RECV_SEND2) ? 1'b1 : 1'b0; // #FLAG
+
+    assign ULPI_ADDR = ULPI_ADDR_r; // #OUTPUT
+    assign ULPI_REG_VAL_W = ULPI_REG_VAL_W_r; // #OUTPUT
+    assign ULPI_PrW = (MAIN_cmd_r == 2'b10) ? MAIN_s_REG_WAIT1 : 1'b0; // #OUTPUT
+    assign ULPI_PrR = (MAIN_cmd_r == 2'b11) ? MAIN_s_REG_WAIT1 : 1'b0; // #OUTPUT
+
+    assign UART_Tx_DATA = UART_Tx_DATA_r; // #OUTPUT
+    assign UART_send = MAIN_s_REG_WAIT2  ||
+                       MAIN_s_RECV_SEND1;   // #OUTPUT
+
+    assign op_stack_pull = MAIN_s_REG_READ  ||
+                           MAIN_s_REG_WRITE ||
+                           MAIN_s_RECV      ||
+                           MAIN_s_REG_SEND;   // #OUTPUT
+
+    assign ULPI_INFO_re = MAIN_s_RECV; // #OUTPUT
+    assign ULPI_DATA_re = MAIN_s_RECV_SEND2 && !UART_Tx_FULL; // #OUTPUT
+    // End of TOP Regs and wires
+
+    // TOP States
+    localparam MAIN_IDLE       = 0;
+    localparam MAIN_REG_READ   = 1;
+    localparam MAIN_REG_WRITE  = 2;
+    localparam MAIN_REG_WAIT1  = 3;
+    localparam MAIN_REG_SEND   = 4;
+    localparam MAIN_REG_WAIT2  = 5;
+    localparam MAIN_RECV       = 6;
+    localparam MAIN_RECV_SEND1 = 7;
+    localparam MAIN_RECV_WAIT  = 8;
+    localparam MAIN_RECV_SEND2 = 9;
+    // End of TOP States
+
+    // TOP controller
+    // States
+    always @(posedge clk `MAIN_CONTROLLER_ASYNC_RESET) begin
+        if(!rst) MAIN_state_r <= MAIN_IDLE;
+        else begin
+            case(MAIN_state_r)
+                MAIN_IDLE: begin
+                    if(!op_stack_empty || force_send) begin
+                        /*
+                           00 -> DATA_RECV
+                           01 -> REG_SEND
+                           10 -> REG_WRITE
+                           11 -> REG_READ
+                         */
+                        if(op_stack_msg[15:14] == 2'b00 && !ULPI_INFO_buff_empty)
+                            MAIN_state_r <= MAIN_RECV;
+                        else if(op_stack_msg[15:14] == 2'b01 || force_send)
+                            MAIN_state_r <= MAIN_REG_SEND;
+                        if(op_stack_msg[15:14] == 2'b10)
+                            MAIN_state_r <= MAIN_REG_WRITE;
+                        else if(op_stack_msg[15:14] == 2'b11)
+                            MAIN_state_r <= MAIN_REG_READ;
+                    end
+                    else
+                        MAIN_state_r <= MAIN_IDLE;
+                end
+                MAIN_REG_READ: begin
+                    MAIN_state_r <= MAIN_REG_WAIT1;
+                end
+                MAIN_REG_WRITE: begin
+                    MAIN_state_r <= MAIN_REG_WAIT1;
+                end
+                MAIN_REG_WAIT1: begin
+                    if(!ULPI_busy) MAIN_state_r <= MAIN_IDLE;
+                    else           MAIN_state_r <= MAIN_REG_WAIT1;
+                end
+                MAIN_REG_SEND: begin
+                    MAIN_state_r <= MAIN_REG_WAIT2;
+                end
+                MAIN_REG_WAIT2: begin
+                    if(UART_Tx_FULL) MAIN_state_r <= MAIN_REG_WAIT2;
+                    else             MAIN_state_r <= MAIN_IDLE;
+                end
+                MAIN_RECV: begin
+                    MAIN_state_r <= MAIN_RECV_SEND1;
+                end
+                MAIN_RECV_SEND1: begin                 
+                    if(UART_Tx_FULL || INFO_count_r != 0) MAIN_state_r <= MAIN_RECV_SEND1;
+                    else             MAIN_state_r <= MAIN_RECV_WAIT;
+                end
+                MAIN_RECV_WAIT: begin       
+                    if(UART_Tx_FULL)
+                        MAIN_state_r <= MAIN_RECV_WAIT;
+                    else begin
+                        if(DATA_count_r == 0)
+                            MAIN_state_r <= MAIN_IDLE;
+                        else
+                            MAIN_state_r <= MAIN_RECV_SEND2;
+                    end
+                end
+                MAIN_RECV_SEND2: begin
+                    if(UART_Tx_FULL || DATA_count_r != 0) MAIN_state_r <= MAIN_RECV_SEND2;
+                    else             MAIN_state_r <= MAIN_IDLE;
+                end
+                default: MAIN_state_r <= MAIN_IDLE;
+            endcase
+        end
+    end
+
+    // REG data & address & CMD
+    always @(negedge clk `MAIN_CONTROLLER_ASYNC_RESET) begin
+        if(!rst) begin
+            MAIN_cmd_r <= 2'b0;
+            ULPI_ADDR_r <= 6'h0;
+            ULPI_REG_VAL_W_r <= 8'b0;
+        end
+        else if(MAIN_s_REG_READ) begin
+            MAIN_cmd_r <= op_stack_msg[15:14];
+            ULPI_ADDR_r <= op_stack_msg[13:8];
+        end
+        else if(MAIN_s_REG_WRITE) begin
+            MAIN_cmd_r <= op_stack_msg[15:14];
+            ULPI_ADDR_r <= op_stack_msg[13:8];
+            ULPI_REG_VAL_W_r <= op_stack_msg[7:0];
+        end
+        else if(MAIN_s_REG_SEND) begin
+            MAIN_cmd_r <= op_stack_msg[15:14];
+        end
+    end
+
+    // UART data
+    always @(negedge clk `MAIN_CONTROLLER_ASYNC_RESET) begin
+        if(!rst)
+            UART_Tx_DATA_r <= 0;
+        else if(MAIN_s_REG_SEND)
+            UART_Tx_DATA_r <= ULPI_REG_VAL_R;
+        // else if(MAIN_s_RECV)
+        //     UART_Tx_DATA_r <= ULPI_USB_INFO_DATA[15:8];
+        else if(MAIN_s_RECV_SEND1 && INFO_count_r == 2)
+            UART_Tx_DATA_r <= {RxCMD_r, DATA_count_r[9:8]};
+        else if(MAIN_s_RECV_SEND1 && INFO_count_r == 1)
+            UART_Tx_DATA_r <= DATA_count_r[7:0];
+        else if(MAIN_s_RECV_SEND2)
+            UART_Tx_DATA_r <= ULPI_USB_DATA;
+    end
+
+    // DATA counter & RxCMD
+    always @(negedge clk `MAIN_CONTROLLER_ASYNC_RESET) begin
+        if(!rst) begin
+            RxCMD_r <= 6'b0;
+            DATA_count_r <= 10'b0;
+        end
+        else if(MAIN_s_RECV) begin
+            RxCMD_r <= ULPI_USB_INFO_DATA[15:10];
+            DATA_count_r <= ULPI_USB_INFO_DATA[9:0];
+        end
+        // else if(MAIN_s_RECV_WAIT)
+        //     DATA_count_r <= DATA_count_r + 1'b1;
+        else if(MAIN_s_RECV_SEND2 && !UART_Tx_FULL)
+            DATA_count_r <= DATA_count_r - 1'b1;
+    end
+
+    // INFO counter
+    always @(negedge clk `MAIN_CONTROLLER_ASYNC_RESET) begin
+        if(!rst)
+            INFO_count_r <= 0;
+        else if(MAIN_s_RECV)
+            INFO_count_r <= 2;
+        else if(MAIN_s_RECV_SEND1 && !UART_Tx_FULL)
+            INFO_count_r <= INFO_count_r - 1'b1;
+    end
+    // End of TOP controllers
+/// End of TOP controller
+
+endmodule
---
+++ ICEstick/USB3300_sniffer/modules/main_controller/tb/main_controller_tb.v
@@ -0,0 +1,227 @@
+/*
+ *
+ * Test bench for the main_controller module
+ * Execute "make gtk" to view the results
+ * 
+ */
+
+`default_nettype none
+`timescale 100ns/10ns
+
+`define ASYNC_RESET
+
+// `include "cells_sim.v"
+
+module main_controller_tb ();
+
+    /// Regs and wires
+    wire op_stack_pull;
+
+    wire ULPI_DATA_re;
+    wire ULPI_INFO_re;
+
+    wire [7:0]ULPI_REG_VAL_W;
+    wire [5:0]ULPI_ADDR;
+    wire ULPI_PrW;
+    wire ULPI_PrR;
+
+    wire [7:0]UART_Tx_DATA;
+    wire UART_send;
+
+
+    reg rst = 0;
+    reg force_send = 0;
+
+    reg [15:0]op_stack_msg = 0;
+    reg op_stack_empty = 1;
+
+    reg [7:0]ULPI_USB_DATA = 0;
+    reg [15:0]ULPI_USB_INFO_DATA = 0;
+    reg ULPI_DATA_buff_empty = 1;
+    reg ULPI_INFO_buff_empty = 1;
+
+    reg ULPI_busy = 0;
+    reg [7:0]ULPI_REG_VAL_R = 0;
+
+    reg UART_Tx_FULL = 0;
+
+
+    reg [32:0]test_num = 0;
+    /// End of Regs and wires
+
+    /// Module under test init
+    main_controller main_controller_mut (                        
+                                         .rst(rst), .clk(clk), .force_send(force_send), // System signals
+                                        
+                                         .op_stack_msg(op_stack_msg), .op_stack_empty(op_stack_empty), .op_stack_pull(op_stack_pull), // Op stack
+
+                                         .ULPI_USB_DATA(ULPI_USB_DATA), .ULPI_USB_INFO_DATA(ULPI_USB_INFO_DATA), // DATA ULPI signals
+                                         .ULPI_DATA_buff_empty(ULPI_DATA_buff_empty), .ULPI_INFO_buff_empty(ULPI_INFO_buff_empty), // DATA ULPI signals
+                                         .ULPI_DATA_re(ULPI_DATA_re), .ULPI_INFO_re(ULPI_INFO_re), // DATA ULPI signals
+
+                                         .ULPI_busy(ULPI_busy), // General ULPI signals
+                                        
+                                         .ULPI_REG_VAL_R(ULPI_REG_VAL_R), .ULPI_REG_VAL_W(ULPI_REG_VAL_W), // Register ULPI signals
+                                         .ULPI_ADDR(ULPI_ADDR), .ULPI_PrW(ULPI_PrW), .ULPI_PrR(ULPI_PrR), // Register ULPI signals
+
+                                         .UART_Tx_FULL(UART_Tx_FULL), .UART_Tx_DATA(UART_Tx_DATA), .UART_send(UART_send) // UART
+                                        );
+    /// End of Module under test init
+
+    /// Clock gen
+    reg clk = 1'b0;
+    always #0.5 clk = ~clk;
+    /// End of Clock gen
+
+    /// Simulation
+    initial begin
+        $dumpfile("./sim/main_controller_tb.vcd");
+        $dumpvars();
+
+        #1 rst = 1;
+
+        // Test 1. Register Read.
+        test_num = 1;
+        op_stack_msg = 16'b11100001_10010110;
+        op_stack_empty = 0;
+        #1.5 op_stack_empty = 1;
+        #1 ULPI_REG_VAL_R = 8'b11001010;
+        #1.5
+
+        // Test 2. Register Write.
+        test_num = 2;
+        op_stack_msg = 16'b10100010_10010110;
+        op_stack_empty = 0;
+        #1.5 op_stack_empty = 1;
+        #2.5
+
+        // Test 3. UART send last register.
+        test_num = 3;
+        op_stack_msg = 16'b01100011_10010110;
+        op_stack_empty = 0;
+        #1.5 op_stack_empty = 1;
+        #2.5
+        
+        // Test 4. Register Read with ULPI_busy.
+        test_num = 4;
+        ULPI_busy = 1;
+        op_stack_msg = 16'b11100001_10010110;
+        op_stack_empty = 0;
+        #1.5 op_stack_empty = 1;
+        #4 ULPI_busy = 0;
+        ULPI_REG_VAL_R = 8'b11001110;
+        #1.5
+        
+        // Test 5. UART send last register with UART_Tx_FULL.
+        test_num = 5;
+        UART_Tx_FULL = 1;
+        op_stack_msg = 16'b01100011_10010110;
+        op_stack_empty = 0;
+        #1.5 op_stack_empty = 1;
+        #4 UART_Tx_FULL = 0;
+        #2.5
+
+        // // Test 6. UART forced send last register.
+        // test_num = 6;
+        // force_send = 1;
+        // ULPI_REG_VAL_R = 8'b01101000;
+        // #1.5 force_send = 0;
+        // #3.5
+
+        // Test 7. RECV send test.
+        test_num = 7;
+        ULPI_USB_DATA = 8'hAC; ULPI_DATA_buff_empty = 0;
+        ULPI_USB_INFO_DATA = 16'b11000100_00000001; ULPI_INFO_buff_empty = 0;
+        op_stack_msg = 16'b00100001_10010110;
+        op_stack_empty = 0;
+        #1.5 op_stack_empty = 1; ULPI_INFO_buff_empty = 1;
+        #4 ULPI_DATA_buff_empty = 1; ULPI_USB_DATA = 8'h00;
+        #2.5
+
+        // Test 8. RECV (more than 1byte) send test.
+        test_num = 8;
+        ULPI_USB_DATA = 8'hAC; ULPI_DATA_buff_empty = 0;
+        ULPI_USB_INFO_DATA = 16'b11000100_00000011; ULPI_INFO_buff_empty = 0;
+        op_stack_msg = 16'b00100001_10010110;
+        op_stack_empty = 0;
+        #1.5 op_stack_empty = 1; ULPI_INFO_buff_empty = 1;
+        #4 ULPI_USB_DATA = 8'hBC;
+        #1 ULPI_USB_DATA = 8'hCC;
+        #1 ULPI_DATA_buff_empty = 1; ULPI_USB_DATA = 8'h00;
+        #2.5
+
+        // Test 9. RECV send empty.!!
+        test_num = 9;
+        ULPI_USB_INFO_DATA = 16'b11000100_00000000; ULPI_INFO_buff_empty = 0;
+        op_stack_msg = 16'b00100001_10010110;
+        op_stack_empty = 0;
+        #1.5 op_stack_empty = 1; ULPI_INFO_buff_empty = 1;
+        #4.5
+
+        // Test 10. RECV send test with TxFull in MAIN_RECV_SEND1.
+        test_num = 10;
+        UART_Tx_FULL = 1;
+        ULPI_USB_DATA = 8'hAC; ULPI_DATA_buff_empty = 0;
+        ULPI_USB_INFO_DATA = 16'b11000100_00000001; ULPI_INFO_buff_empty = 0;
+        op_stack_msg = 16'b00100001_10010110;
+        op_stack_empty = 0;
+        #1.5 op_stack_empty = 1; ULPI_INFO_buff_empty = 1;
+        #2 UART_Tx_FULL = 0;
+        #4 ULPI_DATA_buff_empty = 1; ULPI_USB_DATA = 8'h00;
+        #2.5
+
+        // Test 11. RECV send test with TxFull in MAIN_RECV_SEND2.
+        test_num = 11;
+        ULPI_USB_DATA = 8'hAC; ULPI_DATA_buff_empty = 0;
+        ULPI_USB_INFO_DATA = 16'b11000100_00000001; ULPI_INFO_buff_empty = 0;
+        op_stack_msg = 16'b00100001_10010110;
+        op_stack_empty = 0;
+        #1.5 op_stack_empty = 1; ULPI_INFO_buff_empty = 1;
+        #3 UART_Tx_FULL = 1;
+        #3 UART_Tx_FULL = 0;
+        #1 ULPI_DATA_buff_empty = 1; ULPI_USB_DATA = 8'h00;
+        #2.5
+
+        // Test 12. RECV send (more than 1byte) test with TxFull in MAIN_RECV_SEND2.!!
+        test_num = 12;
+        ULPI_USB_DATA = 8'hAC; ULPI_DATA_buff_empty = 0;
+        ULPI_USB_INFO_DATA = 16'b11000100_00000011; ULPI_INFO_buff_empty = 0;
+        op_stack_msg = 16'b00100001_10010110;
+        op_stack_empty = 0;
+        #1.5 op_stack_empty = 1; ULPI_INFO_buff_empty = 1;
+        #3 UART_Tx_FULL = 1;
+        #3 UART_Tx_FULL = 0;
+        #1 ULPI_USB_DATA = 8'hBC;
+        #1 ULPI_USB_DATA = 8'hCC;
+        #1 ULPI_DATA_buff_empty = 1; ULPI_USB_DATA = 8'h00;
+        #2.5
+
+        // Test 13. RECV send test with TxFull in MAIN_RECV_WAIT.
+        test_num = 13;
+        ULPI_USB_DATA = 8'hAC; ULPI_DATA_buff_empty = 0;
+        ULPI_USB_INFO_DATA = 16'b11000100_00000001; ULPI_INFO_buff_empty = 0;
+        op_stack_msg = 16'b00100001_10010110;
+        op_stack_empty = 0;
+        #1.5 op_stack_empty = 1; ULPI_INFO_buff_empty = 1;
+        #2.5 UART_Tx_FULL = 1;
+        #1.5 UART_Tx_FULL = 0;
+        #2 ULPI_DATA_buff_empty = 1; ULPI_USB_DATA = 8'h00;
+        #2.5
+
+        // Test 14. RECV send test with reset.
+        test_num = 14;
+        ULPI_USB_DATA = 8'hAC; ULPI_DATA_buff_empty = 0;
+        ULPI_USB_INFO_DATA = 16'b11000100_00000001; ULPI_INFO_buff_empty = 0;
+        op_stack_msg = 16'b00100001_10010110;
+        op_stack_empty = 0;
+        #1 rst = 0;
+        op_stack_empty = 1; ULPI_INFO_buff_empty = 1;
+        ULPI_DATA_buff_empty = 1; ULPI_USB_DATA = 8'h00;
+        #1 rst = 1;
+        #2.5
+
+        $finish;
+    end
+    /// End of Simulation
+
+endmodule
